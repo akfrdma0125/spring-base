bdfb1f03f880195f73488fa114f595a7b0ca30be
akfrdma0125 <nogain1215@gmail.com>
2023-12-28 22:46:45 +0900

    feat(#2): 계산기 프로그램 초안

--- src/main/java/com/example/base/BaseApplication.java
+++ src/main/java/com/example/base/BaseApplication.java
@@ -3,11 +3,38 @@ package com.example.base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class BaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BaseApplication.class, args);
+//        SpringApplication.run(BaseApplication.class, args);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two numbers and an operator like this: 1 + 2");
+        String result = scanner.nextLine();
+        String[] parts = result.split(" ");
+        long num1 = Long.parseLong(parts[0]);
+        long num2 = Long.parseLong(parts[2]);
+        String operator = parts[1];
+        long answer;
+        switch (operator) {
+            case "+":
+                answer = num1 + num2;
+                break;
+            case "-":
+                answer = num1 - num2;
+                break;
+            case "*":
+                answer = num1 * num2;
+                break;
+            case "/":
+                answer = num1 / num2;
+                break;
+            default:
+                throw new InvalidOperatorException();
+        }
+        System.out.println(answer);
     }
 
 }
