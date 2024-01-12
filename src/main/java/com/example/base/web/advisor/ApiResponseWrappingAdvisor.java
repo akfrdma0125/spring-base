3a2d893c3f3ac6cd96b8644ee567cb9655220920
akfrdma0125 <nogain1215@gmail.com>
2024-01-12 13:09:36 +0900

    feat(#3): ApiResposeWrappingAdvisor 코드 수정

--- src/main/java/com/example/base/web/advisor/ApiResponseWrappingAdvisor.java
+++ src/main/java/com/example/base/web/advisor/ApiResponseWrappingAdvisor.java
@@ -1,12 +1,10 @@
 package com.example.base.web.advisor;
 
 import com.example.base.common.util.JsonUtils;
+import com.example.base.web.dto.ApiResponse;
 import com.example.base.web.dto.ApiResponseGenerator;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.GenericTypeResolver;
 import org.springframework.core.MethodParameter;
-import org.springframework.core.ResolvableType;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
@@ -21,33 +19,13 @@ import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-import java.lang.reflect.Type;
 import java.util.Objects;
 
 @Slf4j
 @RestControllerAdvice(basePackages = "com.example.base")
 public class ApiResponseWrappingAdvisor implements ResponseBodyAdvice<Object> {
-    private Type getGenericType(MethodParameter returnType) {
-        if (HttpEntity.class.isAssignableFrom(returnType.getParameterType())) {
-            return ResolvableType.forType(returnType.getGenericParameterType()).getGeneric().getType();
-        } else {
-            return returnType.getGenericParameterType();
-        }
-    }
-
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
-        Type type = GenericTypeResolver.resolveType(getGenericType(returnType),
-                returnType.getContainingClass());
-
-        // void 타입은 예외처리
-        if (Void.class.getName().equals(type.getTypeName())) {
-            return false;
-        }
-
-        //ByteArrayHttpMessageConverter, ResourceHttpMessageConverter는 예외처리
-        //ByteArrayHttpMessageConverter: 이미지나 기타 이진 데이터 다룰 때 유용
-        //ResourceHttpMessageConverter: 파일 업로드/다운로드
         return !converterType.isAssignableFrom(ByteArrayHttpMessageConverter.class) &&
                 !converterType.isAssignableFrom(ResourceHttpMessageConverter.class);
     }
@@ -59,32 +37,60 @@ public class ApiResponseWrappingAdvisor implements ResponseBodyAdvice<Object> {
                                   @NonNull Class<? extends HttpMessageConverter<?>> selectedConverterType,
                                   @NonNull ServerHttpRequest request,
                                   @NonNull ServerHttpResponse response) {
+        String path = request.getURI().getPath();
 
         HttpStatus responseStatus = HttpStatus.valueOf(
                 ((ServletServerHttpResponse) response).getServletResponse().getStatus()
         );
 
-        if (Objects.isNull(body)) {
-            if (responseStatus.isError()) {
-                return ApiResponseGenerator.fail();
-            }
-            return HttpStatus.CREATED.equals(responseStatus)? ApiResponseGenerator.created(): ApiResponseGenerator.success();
-        }
 
-        var apiResponse = responseStatus.isError() ? ApiResponseGenerator.fail(body) : ApiResponseGenerator.success(body);
-        log.trace("[ApiResponse] {}", apiResponse);
+       var apiResponse = generateApiResponse(path, body, response, responseStatus);
+       logResponse(apiResponse, responseStatus);
 
-        // StringHttpMessageConverter 및 하위 클래스에 json 처리
-        if (selectedConverterType.isAssignableFrom(StringHttpMessageConverter.class)) {
-            try {
-                response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-                return JsonUtils.toJson(apiResponse);
-            } catch (JsonUtils.JsonEncodeException jpe) {
-                log.warn("JSON 처리 중 오류 발생", jpe);
-                throw new ApiResponseJsonProcessingException(jpe);
-            }
-        }
+       if (selectedConverterType.isAssignableFrom(StringHttpMessageConverter.class)) {
+           return convertToJson(response, apiResponse);
+       }
 
         return apiResponse;
+    }
+
+    private static String convertToJson(ServerHttpResponse response, ApiResponse<?> apiResponse) {
+        try {
+            response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+            return JsonUtils.toJson(apiResponse);
+        } catch (JsonUtils.JsonEncodeException jpe) {
+            log.warn("JSON 처리 중 오류 발생", jpe);
+            throw new ApiResponseJsonProcessingException(jpe);
+        }
+    }
+
+    private ApiResponse<?> generateApiResponse(String path, Object body, ServerHttpResponse response, HttpStatus responseStatus) {
+        if (responseStatus.isError()) {
+            return handleError(path, body);
+        }
+        if (Objects.isNull(body)) {
+            response.setStatusCode(HttpStatus.CREATED);
+            return ApiResponseGenerator.created(path);
+        }
+        return ApiResponseGenerator.success(path, body);
+    }
+
+    private void logResponse(ApiResponse<?> response, HttpStatus status) {
+        if (status.isError()) {
+            log.error("[ErrorResponse] {}", response);
+        } else{
+            log.trace("[ApiResponse] {}", response);
+        }
+    }
+
+    private ApiResponse<?> handleError(String path, Object body){
+        if (Objects.isNull(body)){
+            return ApiResponseGenerator.fail(path);
+        }
+        if (body instanceof ApiResponse) {
+            ApiResponse<?> errorResponse = (ApiResponse<?>) body;
+            return ApiResponseGenerator.of(path, errorResponse.code(), errorResponse.message(), errorResponse.data());
+        }
+        return ApiResponseGenerator.fail(path, body);
     }
 }
